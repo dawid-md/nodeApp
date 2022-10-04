8a6f7d7f137cba7ad5633435b28aa628d1afbc0f
Dawid Mądrzyk <maddawid@gmail.com>
2022-10-05 00:44:39 +0200

    changes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+import { readFile, readFileSync }  from "fs"
+import express from "express"
+
+const app = express()
+
+async function getText() {
+    let xd = await readFile('./data.txt', 'utf-8', (err, txt) => txt
+    )
+    console.log(xd);
+}
+
+
+
+app.get('/', (req, res) => {
+    res.send('dupa')
+})
+
+app.get('/a', (req, res) => {
+    res.send('hello you')
+})
+
+app.listen(3000, () => console.log('port is listening'))
