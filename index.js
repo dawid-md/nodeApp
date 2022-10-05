ac6c45efdf763afa848146fcef373cd8d33f061c
Dawid Mądrzyk <maddawid@gmail.com>
2022-10-05 21:57:25 +0200

    readfile sync

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { readFile, readFileSync }  from "fs"
 import express from "express"
+import { resolve } from "path"
 
 const app = express()
 
 async function getText() {
-    let xd = await readFile('./data.txt', 'utf-8', (err, txt) => txt
-    )
-    console.log(xd);
+    let rfile = readFileSync('./data.txt', 'utf-8', (err, data) => {} )
+    return rfile
 }
 
-
+let xx = await getText()
+console.log(xx);
 
 app.get('/', (req, res) => {
     res.send('dupa')
