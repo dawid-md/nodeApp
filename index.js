aa5823e3e25b34b3b55c00df92292f514f74ef00
Dawid Mądrzyk <maddawid@gmail.com>
2022-10-09 23:12:21 +0200

    sending data from mongodb to browser

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import { readFile, readFileSync }  from "fs"
 import express from "express"
 import { resolve } from "path"
-
 import { MongoClient } from "mongodb" 
-const dbname = 'mongo-test'
-const url = 'mongodb://127.0.0.1:27017'
-let resu = ''
-
-    MongoClient.connect(url, {}, (error, client) => {
-        if (error) console.log('cant connect to database');
-        console.log('connection ok');
-        const mdb = client.db(dbname)
-
-         mdb.collection('users').find().toArray((error, results) => {
-             //console.log(results[0]['name']);
-             //resu = results[0]['name']
-         })
-         
-    })
-
 
 const app = express()
+
+const dbname = 'mongo-test'
+const uri = 'mongodb://127.0.0.1:27017'
+let resu = ''
+
+const client = new MongoClient(uri);
+const database = client.db(dbname);
+
+async function getDatafromBase() {
+    try {
+      const users = database.collection('users');
+      const query = { 'name': 'Johnny' };
+      const user = await users.findOne(query);
+      return user['sex']
+    } finally {
+      // await client.close();      // Ensures that the client will close when you finish/error
+    }
+  }
 
 function getText() {
     let rfile = readFileSync('./data.txt', 'utf-8', (err, data) => {} )
@@ -28,9 +29,6 @@ function getText() {
 }
 
 async function getData() {
-    // let promise = new Promise((resolve, reject) => {
-    //     setTimeout(() => resolve("done!"), 1000)
-    //   });
     let result = getText()  
     return result
 }
@@ -44,7 +42,12 @@ app.get('/a', async function(req, res){
     res.send(resu)
 })
 
-app.listen(3000, () => console.log('port is listening'))
+app.get('/b', async function(req, res){
+    let sayu = await getDatafromBase()
+    res.send(sayu)
+})
+
+app.listen(3000, () => console.log('..................'))
 
 
 
@@ -53,3 +56,16 @@ app.listen(3000, () => console.log('port is listening'))
         //      name: 'Zosia',
         //      age: 32
         //  })
+
+            // MongoClient.connect(url, {}, (error, client) => {
+    //     if (error) console.log('cant connect to database');
+    //     console.log('connection ok');
+    //     const mdb = client.db(dbname)
+
+    //     mdb.collection('users').find().toArray((error, result) => {
+    //          //console.log(results[0]['name']);
+    //          //resu = results[0]['name']
+    //      })
+    // })
+
+    //run().catch(console.dir);
